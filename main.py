217f3aa48cff25f9c07354f99e100b5d1dc070ce
kapoel <fokoukapoel@gmail.com>
2025-12-02 20:03:41 +0100

    feature : create function cesar_sypher

---
+++ main.py
@@ -0,0 +1,20 @@
+# useful functions used in the project
+
+# ord : is a native function of python that take a character like 'a' or 'B' and give you its 
+#       ordinar value in ascii table
+
+# chr : it's the reverse of the function 'ord'. it means that it takes a number in parameter 
+#       and give you the correspondant character in ascii table
+ 
+# modulo i 
+
+def cesar_sypher(message,key):
+    
+    crypted_message = ""
+    for character in message:
+        position_character_crypted = (ord(character) + key) % 1_114_112
+        crypted_character = chr(position_character_crypted)
+        crypted_message += crypted_character
+    return crypted_message
+    
+print(cesar_sypher(message="bouz",key=4))
