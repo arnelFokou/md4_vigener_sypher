cbe5b669c857df752194c7c26d53b16d69fbcb44
kapoel <fokoukapoel@gmail.com>
2025-12-02 22:22:28 +0100

    bug fix: correction of function cesar_sypher

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-# useful functions used in the project
 
-# ord : is a native function of python that take a character like 'a' or 'B' and give you its 
-#       ordinar value in ascii table
+import string
 
-# chr : it's the reverse of the function 'ord'. it means that it takes a number in parameter 
-#       and give you the correspondant character in ascii table
- 
-# modulo i 
+# liste_characters variable contains all lowercase letters from a to z
+liste_characters = string.ascii_lowercase
+
+def ord(character):
+    # return the ordinar value of the character in liste_characters variable
+    return liste_characters.index(character)
+
+def chr(number):
+    # return the character correspondant to the number in liste_characters variable
+    return liste_characters[number%26]
 
 def cesar_sypher(message,key):
-    
+   #return the crypted message using cesar sypher method with a key
     crypted_message = ""
-    for character in message:
-        position_character_crypted = (ord(character) + key) % 1_114_112
-        crypted_character = chr(position_character_crypted)
-        crypted_message += crypted_character
+    for char in message.lower():
+        crypted_char = chr((ord(char) + key))
+        crypted_message += crypted_char
     return crypted_message
     
 def cesar_unsypher(message,key):
+    #return the decrypted message using cesar sypher method with a key
     return cesar_sypher(message,-key)
 
-print(cesar_unsypher(message="fsy~",key=4))
+print(cesar_unsypher(message="psmgdsy",key=4))
+# print(string.ascii_letters)
+
+
