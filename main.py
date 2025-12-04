ac0bbf879e7bbcdcf74deef4c0ecba5780452104
kapoel <fokoukapoel@gmail.com>
2025-12-04 21:35:51 +0100

    refactor : optimizing spatial complexity

--- main.py
+++ main.py
@@ -16,35 +16,31 @@ def chr(number):
 def cesar_sypher(message,key,cipher=True):
    #cipher = True means encrypt the message and False means decrypt the message
     key = key if cipher else -key
-    message_result = ""
-    for char in message.lower():
-        crypted_char = chr((ord(char) + key))
-        message_result += crypted_char
-    return message_result
+
+    message_result = [chr((ord(char) + key)) for char in message.lower()]
+    return "".join(message_result)
     
 
 #return all possible decrypted messages using cesar unsypher function with all possible keys
 def hack_cesar_sypher(message_crypted):
-   possible_messages=[]
-   for possible_key in range(len(liste_characters)):
-       possible_messages.append(cesar_sypher(message_crypted,possible_key,cipher = False))
+   possible_messages=[cesar_sypher(message_crypted,possible_key,cipher = False) for possible_key in range(len(liste_characters)) ]
    return possible_messages
 
 #return a message crypted or decrypted function of the parameter cipher 
 def vigenere_sypher(message,key,cipher=True):
     cipher = cipher if cipher else False
+    list_of_keys=[ord(char) for char in key]
 
-    message_result = ""    
-
+    message_result = []
     for index_char,char in enumerate(message):
-        current_key = ord( key[index_char % len(key)] ) 
+        current_key = list_of_keys[index_char % len(list_of_keys)]
         crypted_char = cesar_sypher(char,current_key,cipher=cipher)
-        message_result += crypted_char
-    return message_result
+        message_result.append(crypted_char)
+    return "".join(message_result)
 
 
-print(vigenere_sypher(message="jftyitvrmjhrooherdpr",key='abbe',cipher = False))  
-
+print(vigenere_sypher(message="bonjour",key='allo',cipher = True))
+#bzyxofc
 
 
 
